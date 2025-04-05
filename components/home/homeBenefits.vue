374b014628fb4a2e86095b2e3aeb23d2d8dcf193
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-05 19:53:09 +0700

    test svg

--- components/home/homeBenefits.vue
+++ components/home/homeBenefits.vue
@@ -1,3 +1,7 @@
+<script>
+    import TimeIcon from '~/assets/svg/TimeIcon.svg'
+</script>
+
 <template>
     <div class="bg-[#D9D9D9] h-fit md:pb-[200px] pt-2">
         <div class="pt-20 pb-10 flex justify-center md:justify-start">
@@ -19,11 +23,11 @@
                             <div class="">
                                 <NuxtImg src="/Logo1.png" class="md:h-[103px] md:w-[75px] h-[75px] w-[55px]" />
                             </div>
-                            <div class="ml-5">
-                                <div
+                            <div class="">
+                                <p
                                     class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
-                                    top-notch</div>
-                                <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
+                                    top-notch</p>
+                                <p class="italic font-[300] md:text-[32px] text-[15px]">Quality</p>
                             </div>
                         </div>
                         <div
@@ -43,15 +47,15 @@
                 <div
                     class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] py-10 px-[70px] md:w-[550px] md:h-fit rounded-[8px] shadow-lg md:translate-y-[-10rem] md:translate-x-[-7rem] flex items-center justify-center">
                     <div class="md:w-[350px] w-[220px]">
-                        <div class="flex gap-6 items-center">
+                        <div class="flex gap-7 items-center">
                             <div class="">
-                                <NuxtImg src="/Time.png" class="md:h-[100px] md:w-[91px] h-[70px] w-[70px]" />
+                                <nuxt-img src="price.svg" class="md:h-[100px] md:w-[91px] h-[70px] w-[70px]" />
                             </div>
-                            <div class="ml-5">
-                                <div
-                                    class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
-                                    on time</div>
-                                <div class="italic font-[300] md:text-[32px] text-[15px]">Delivery</div>
+                            <div class="">
+                                <p
+                                    class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px] leading-[50px]">
+                                    on time</p>
+                                <p class="italic font-[300] md:text-[32px] text-[15px] leading-[50px]">Delivery</p>
                             </div>
                         </div>
                         <div
@@ -72,11 +76,11 @@
                             <div class="">
                                 <NuxtImg src="/Save.png" class="md:h-[75px] md:w-[105px] h-[70px] w-[80px]" />
                             </div>
-                            <div class="ml-5">
-                                <div
+                            <div class="">
+                                <p
                                     class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
-                                    cost</div>
-                                <div class="italic font-[300] md:text-[32px] text-[15px]">Efficiency</div>
+                                    cost</p>
+                                <p class="italic font-[300] md:text-[32px] text-[15px]">Efficiency</p>
                             </div>
                         </div>
                         <div
@@ -98,11 +102,11 @@
                                 <div class="">
                                     <NuxtImg src="/Hand.png" class="md:h-[100px] md:w-[100px] h-[70px] w-[70px]" />
                                 </div>
-                                <div class="ml-5">
-                                    <div
+                                <div class="">
+                                    <p
                                         class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
-                                        seamless</div>
-                                    <div class="italic font-[300] md:text-[32px] text-[15px]">Collaboration</div>
+                                        seamless</p>
+                                    <p class="italic font-[300] md:text-[32px] text-[15px]">Collaboration</p>
                                 </div>
                             </div>
                         </div>
