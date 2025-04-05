b0a618822afdf26cdcdee267b1d51d7e8ecd39c5
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-05 21:01:48 +0700

    fix : Fix Responsive button QUOTE ME

--- components/home/quoteButton.vue
+++ components/home/quoteButton.vue
@@ -15,7 +15,7 @@ onUnmounted(() => {
 <template>
     <div>
         <UButton v-if="isScrolled"
-            class="block fixed bottom-10 right-10 bg-[#980C0C] text-[#F5F5F5] md:px-4 md:py-3 rounded-[10px] shadow-lg hover:bg-[#980C0C] transition z-50">
+            class="block fixed md:bottom-10 md:right-10 bottom-6 right-6 bg-[#980C0C] text-[#F5F5F5] md:px-4 md:py-3 rounded-[10px] shadow-lg hover:bg-[#980C0C] transition z-50">
             <div class="uppercase text-[#F5F5F5] md:font-[500] font-[400]  md:text-[18px] text-[13px] leading-[16px] px-[10px] py-[5px]">quote now
             </div>
         </UButton>
