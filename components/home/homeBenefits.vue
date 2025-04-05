b6f24879740f60ad5c7cca1a933dd41d7fc5939a
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-05 19:00:43 +0700

    fix : Fix linh ta linh tinh lung linh

--- components/home/homeBenefits.vue
+++ components/home/homeBenefits.vue
@@ -2,20 +2,22 @@
     <div class="bg-[#D9D9D9] h-fit md:pb-[200px] pt-2">
         <div class="pt-20 pb-10 flex justify-center md:justify-start">
             <div class="md:w-[1000px] md:px-[200px] w-[300px]">
-                <div class="md:text-[32px] text-[16px] leading-[150%] font-[600] text-[#000000] text-center md:text-left">What is the
+                <div
+                    class="md:text-[32px] text-[16px] leading-[150%] font-[600] text-[#000000] text-center md:text-left">
+                    What is the
                     benefits of</div>
                 <div class="md:text-[64px] text-[30px] font-[600] text-[#8D7662] mt-2 text-center md:text-left">
                     partnering with Nomo Render?</div>
             </div>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2">
-            <div class="md:pl-[200px] md:transform px-10 pb-10">
+            <div class="md:pl-[200px] md:transform px-[70px] pb-10">
                 <div
-                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg">
-                    <div class="md:w-[350px] md:h-[483px]]">
+                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg flex items-center justify-center py-10">
+                    <div class="md:w-[350px] md:h-[483px] w-[220px] h-auto">
                         <div class="flex gap-6 items-center">
                             <div class="">
-                                <NuxtImg src="/Logo1.png" class="md:h-[103px] md:w-[75px] h-[70px] w-[70px]"/>
+                                <NuxtImg src="/Logo1.png" class="md:h-[103px] md:w-[75px] h-[75px] w-[55px]" />
                             </div>
                             <div class="ml-5">
                                 <div
@@ -24,7 +26,8 @@
                                 <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
                             </div>
                         </div>
-                        <div class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
+                        <div
+                            class="h-[190px] md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
                             We deliver diverse and realistic 3D images with <span class="font-[500]">precision</span>
                             and
                             <span class="font-[500]">consistency</span>. Every rendering
@@ -36,13 +39,13 @@
                 </div>
             </div>
 
-            <div class="md:pl-[200px] transform px-10 pb-10">
+            <div class="md:pl-[200px] transform px-[70px] pb-10">
                 <div
-                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg md:translate-y-[-7rem] md:translate-x-[-7rem]">
-                    <div class="md:w-[350px] md:h-[483px]">
+                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg md:translate-y-[-7rem] md:translate-x-[-7rem] flex items-center justify-center">
+                    <div class="md:w-[350px] md:h-[483px] w-[220px] h-auto">
                         <div class="flex gap-6 items-center">
                             <div class="">
-                                <NuxtImg src="/Time.png" class="md:h-[100px] md:w-[91px] h-[70px] w-[70px]"/>
+                                <NuxtImg src="/Time.png" class="md:h-[100px] md:w-[91px] h-[70px] w-[70px]" />
                             </div>
                             <div class="ml-5">
                                 <div
@@ -51,7 +54,8 @@
                                 <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
                             </div>
                         </div>
-                        <div class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
+                        <div
+                            class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
                             We deliver diverse and realistic 3D images with <span class="font-[500]">precision</span>
                             and
                             <span class="font-[500]">consistency</span>. Every rendering
@@ -63,24 +67,23 @@
 
                 </div>
             </div>
-            <div class="md:pl-[200px] transform px-10 pb-10">
+            <div class="md:pl-[200px] transform px-[70px] pb-10">
                 <div
-                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg md:translate-y-[6rem]">
-                    <div class="md:w-[350px] md:h-[483px]">
-                        <div class="flex">
+                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg md:translate-y-[6rem] flex items-center justify-center">
+                    <div class="md:w-[350px] md:h-[483px] w-[220px] h-auto">
                             <div class="flex gap-6 items-center">
-                            <div class="">
-                                <NuxtImg src="/Save.png" class="md:h-[75px] md:w-[105px] h-[70px] w-[70px]"/>
+                                <div class="">
+                                    <NuxtImg src="/Save.png" class="md:h-[75px] md:w-[105px] h-[70px] w-[80px]" />
+                                </div>
+                                <div class="ml-5">
+                                    <div
+                                        class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
+                                        top-notch</div>
+                                    <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
+                                </div>
                             </div>
-                            <div class="ml-5">
-                                <div
-                                    class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
-                                    top-notch</div>
-                                <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
-                            </div>
-                        </div>
-                        </div>
-                        <div class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
+                        <div
+                            class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
                             We deliver diverse and realistic 3D images with <span class="font-[500]">precision</span>
                             and
                             <span class="font-[500]">consistency</span>. Every rendering
@@ -92,24 +95,25 @@
 
                 </div>
             </div>
-            <div class="md:pl-[200px] transform px-10 pb-10">
+            <div class="md:pl-[200px] transform px-[70px] pb-10">
                 <div
-                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg md:translate-y-[-1rem] md:translate-x-[-7rem]">
-                    <div class="md:w-[350px] md:h-[483px]">
+                    class="bg-[#D8C3A566] md:px-[100px] md:py-[65px] px-[70px] py-[70px] md:w-[550px] md:h-[614px] rounded-[8px] shadow-lg md:translate-y-[-1rem] md:translate-x-[-7rem] flex items-center justify-center">
+                    <div class="md:w-[350px] md:h-[483px] w-[220px] h-auto">
                         <div class="flex">
                             <div class="flex gap-6 items-center">
-                            <div class="">
-                                <NuxtImg src="/Hand.png" class="md:h-[100px] md:w-[100px] h-[70px] w-[70px]"/>
-                            </div>
-                            <div class="ml-5">
-                                <div
-                                    class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
-                                    top-notch</div>
-                                <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
+                                <div class="">
+                                    <NuxtImg src="/Hand.png" class="md:h-[100px] md:w-[100px] h-[70px] w-[70px]" />
+                                </div>
+                                <div class="ml-5">
+                                    <div
+                                        class="uppercase md:text-[25px] md:font-[900] font-[800] text-[#8D7662] text-[16px]">
+                                        top-notch</div>
+                                    <div class="italic font-[300] md:text-[32px] text-[15px]">Quality</div>
+                                </div>
                             </div>
                         </div>
-                        </div>
-                        <div class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
+                        <div
+                            class="md:text-[25px] text-[15px] font-[300] mt-[30px] text-justify md:leading-[200%] leading-[150%]">
                             We deliver diverse and realistic 3D images with <span class="font-[500]">precision</span>
                             and
                             <span class="font-[500]">consistency</span>. Every rendering
