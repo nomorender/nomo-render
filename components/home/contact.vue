6cd6d1a989830302af2e7f7f684154a84fabd7dd
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-06 12:25:38 +0700

    add : responsive

--- components/home/contact.vue
+++ components/home/contact.vue
@@ -27,62 +27,69 @@ async function onError(event: FormErrorEvent) {
 
 <template>
     <div class="bg-[#D9D9D9] py-10 md:py-20">
-        <div class="md:px-8">
-            <div>
-                <p class="text-center text-xl md:text-3xl uppercase">contact</p>
-                <div
-                    class="text-center font-semibold text-3xl md:text-[64px] leading-[100%] mb-5 md:mb-20 text-[#8D7662] px-6 md:px-20">
-                    Let’s bring your vision to life!
-                </div>
-            </div>
-            <div class="block lg:flex lg:gap-[100px] lg:px-[150px]">
-                <div class="lg:w-[50%]">
-                    <p
-                        class="text-justify font-[300] italic text-[15px] md:text-[25px] leading-[180%] md:leading-[150%] px-7 md:px-0 max-w-4xl mx-auto">
-                        Have a project in mind? Whether you need stunning 3D renderings, detailed architectural visuals,
-                        or custom design solutions, we’re here to help. Contact us, and let’s discuss how we can
-                        transform your ideas into reality.
-                    </p>
-                    <div class="grid grid-cols-2 gap-2 mt-10 place-items-center md:place-items-start w-full px-0 mx-0">
-                        <NuxtImg src="/Contact1.png"
-                            class="w-full max-w-[190px] h-auto md:max-w-[335px] object-cover" />
-                        <NuxtImg src="/Contact2.png"
-                            class="w-full max-w-[190px] h-auto md:max-w-[335px] object-cover" />
+        <div class="md:px-8 flex items-center justify-center">
+            <div class="w-[1072px]">
+                <div>
+                    <p class="text-center text-xl md:text-3xl uppercase">contact</p>
+                    <div
+                        class="text-center font-semibold text-3xl md:text-[64px] leading-[100%] mb-5 md:mb-20 text-[#8D7662] px-6 md:px-20">
+                        Let’s bring your vision to life!
                     </div>
                 </div>
-
-                <div class="lg:w-[50%] mt-6 lg:mt-0">
-                    <UForm id="contactForm" :validate="validate" :state="state" class="space-y-4 px-4 md:px-0"
-                        @submit="onSubmit" @error="onError">
-                        <UFormGroup>
-                            <UInput padded required color="white" variant="none" placeholder="Name (required)"
-                                v-model="state.name" size="xl" class="bg-[#C6C6C6] px-2 rounded-[8px] shadow-lg w-full"
-                                :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }" />
-                        </UFormGroup>
-
-                        <UFormGroup>
-                            <UInput padded required color="white" variant="none" placeholder="Email (required)"
-                                v-model="state.email" size="xl" class="bg-[#C6C6C6] px-2 rounded-[8px] shadow-lg w-full"
-                                :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }" />
-                        </UFormGroup>
-
-                        <UFormGroup>
-                            <UTextarea padded required color="white" variant="none" placeholder="Describe your project"
-                                v-model="state.description" size="xl"
-                                class="bg-[#C6C6C6] px-2 rounded-[8px] shadow-lg w-full"
-                                :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }"
-                                :rows="8" md:rows="10" />
-                        </UFormGroup>
-
-                        <div class="px-4 md:px-0 mx-auto flex justify-center lg:block">
-                            <UButton color="gray" size="xl" variant="solid" type="submit" form="contactForm"
-                                class="bg-gradient-to-r from-[#8D7662] to-[#27211B] lg:px-6 lg:py-4 px-10 py-6 hover:bg-[#90755e] mt-6 md:mt-10 rounded-[8px] w-full md:w-auto max-w-xs">
-                                <div class="w-full flex justify-center items-center uppercase text-[#F5F5F5] text-[16px] md:text-[20px]">
-                                    let’s work together!
-                                </div>
-                            </UButton>
+                <div class="block lg:flex lg:gap-[100px]">
+                    <div class="lg:w-[50%]">
+                        <p
+                            class="text-justify font-[300] italic text-[15px] md:text-[25px] leading-[180%] md:leading-[150%] px-7 md:px-0 max-w-4xl mx-auto">
+                            Have a project in mind? Whether you need stunning 3D renderings, detailed architectural
+                            visuals,
+                            or custom design solutions, we’re here to help. Contact us, and let’s discuss how we can
+                            transform your ideas into reality.
+                        </p>
+                        <div
+                            class="grid grid-cols-2 gap-2 mt-10 place-items-center md:place-items-start w-full px-0 mx-0">
+                            <NuxtImg src="/Contact1.png"
+                                class="w-full max-w-[190px] h-auto md:max-w-[335px] object-cover" />
+                            <NuxtImg src="/Contact2.png"
+                                class="w-full max-w-[190px] h-auto md:max-w-[335px] object-cover" />
                         </div>
-                    </UForm>
+                    </div>
+
+                    <div class="lg:w-[50%] mt-6 lg:mt-0">
+                        <UForm id="contactForm" :validate="validate" :state="state" class="space-y-4 px-4 md:px-0"
+                            @submit="onSubmit" @error="onError">
+                            <UFormGroup>
+                                <UInput padded required color="white" variant="none" placeholder="Name (required)"
+                                    v-model="state.name" size="xl"
+                                    class="bg-[#C6C6C6] px-2 rounded-[8px] shadow-lg w-full"
+                                    :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }" />
+                            </UFormGroup>
+
+                            <UFormGroup>
+                                <UInput padded required color="white" variant="none" placeholder="Email (required)"
+                                    v-model="state.email" size="xl"
+                                    class="bg-[#C6C6C6] px-2 rounded-[8px] shadow-lg w-full"
+                                    :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }" />
+                            </UFormGroup>
+
+                            <UFormGroup>
+                                <UTextarea padded required color="white" variant="none"
+                                    placeholder="Describe your project" v-model="state.description" size="xl"
+                                    class="bg-[#C6C6C6] px-2 rounded-[8px] shadow-lg w-full"
+                                    :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }"
+                                    :rows="8" md:rows="10" />
+                            </UFormGroup>
+
+                            <div class="px-4 md:px-0 mx-auto flex justify-center lg:block">
+                                <UButton color="gray" size="xl" variant="solid" type="submit" form="contactForm"
+                                    class="bg-gradient-to-r from-[#8D7662] to-[#27211B] lg:px-6 lg:py-4 px-10 py-6 hover:bg-[#90755e] mt-6 md:mt-10 rounded-[8px] w-full md:w-auto max-w-xs">
+                                    <div
+                                        class="w-full flex justify-center items-center uppercase text-[#F5F5F5] text-[16px] md:text-[20px]">
+                                        let’s work together!
+                                    </div>
+                                </UButton>
+                            </div>
+                        </UForm>
+                    </div>
                 </div>
             </div>
         </div>
