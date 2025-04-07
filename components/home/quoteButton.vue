57f343f7b3bd493a2305138c69a8cc515210d45a
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-07 12:12:18 +0700

    fx

--- components/home/quoteButton.vue
+++ components/home/quoteButton.vue
@@ -43,7 +43,7 @@ async function onError(event: FormErrorEvent) {
 <template>
     <div>
         <UButton v-if="isScrolled" @click="isOpen = true"
-            class="block fixed md:bottom-10 md:right-10 bottom-6 right-6 bg-[#980C0C] text-[#F5F5F5] md:px-4 md:py-3 rounded-[10px] shadow-lg hover:bg-[#980C0C] transition z-50">
+            class="block fixed md:bottom-10 md:right-10 bottom-6 right-6 bg-[#980C0C] text-[#F5F5F5] md:px-2 md:py-2 rounded-[10px] shadow-lg hover:bg-[#980C0C] transition z-50">
             <div
                 class="uppercase text-[#F5F5F5] md:font-[500] font-[400] md:text-[18px] text-[13px] leading-[16px] px-[10px] py-[5px]">
                 quote now
