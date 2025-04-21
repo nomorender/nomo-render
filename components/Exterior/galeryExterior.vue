e1c6254461554ac738b889dfd0e3ad366b6ea8f2
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-21 22:10:52 +0700

    ADD : Gallery

--- components/Exterior/galeryExterior.vue
+++ components/Exterior/galeryExterior.vue
@@ -1,38 +1,362 @@
 <template>
-  <div class="flex justify-center items-center ">
-    <div class="columns-3 gap-2 space-y-2">
-      <NuxtImg
-        src="/Extorior/1.png"
-        class="w-full h-[706.98828125px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/4.png"
-        class="w-full h-[594.13427734375px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/2.png"
-        class="w-full h-[466.98828125px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/5.png"
-        class="w-full h-[395.7802734375px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/8.png"
-        class="w-full h-[432.4881591796875px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/3.png"
-        class="w-full h-[352.85400390625px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/7.png"
-        class="w-full h-[588.2685546875px] break-inside-avoid object-cover object-center"
-      />
-      <NuxtImg
-        src="/Extorior/6.png"
-        class="w-full h-[354.13427734375px] break-inside-avoid object-cover object-center"
-      />
+  <div class="flex justify-center items-center">
+    <div>
+      <div class="columns-3 gap-2 space-y-2">
+        <div class="relative group w-full h-[706.98828125px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/1.png"
+            class="w-full h-full object-cover object-center"
+          />
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[595px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/4.png"
+            class="w-full h-[595px] break-inside-avoid object-cover object-center"
+          />
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[467px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/2.png"
+            class="w-full h-[467px] break-inside-avoid object-cover object-center"
+          />
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[396px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/5.png"
+            class="w-full h-[396px] break-inside-avoid object-cover object-center"
+          />
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[432px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/8.png"
+            class="w-full h-[432px] break-inside-avoid object-cover object-center"
+          />
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[353px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/3.png"
+            class="w-full h-[353px] break-inside-avoid object-cover object-center"
+          />
+
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[588.2685546875px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/7.png"
+            class="w-full h-[588.2685546875px] break-inside-avoid object-cover object-center"
+          />
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="relative group w-full h-[354.13427734375px] cursor-pointer">
+          <NuxtImg
+            src="/Extorior/6.png"
+            class="w-full h-[354.13427734375px] break-inside-avoid object-cover object-center"
+          />
+
+          <div
+            class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+          >
+            <div>
+              <div class="text-[#FFFFFF] text-justify text-[20px]">
+                <div class="flex justify-between">
+                  <div class="font-[600] leading-[200%]">Name</div>
+                  <div class="flex justify-center items-center">
+                    <Icon
+                      name="heroicons:arrows-pointing-out-16-solid"
+                      class="size-5 text-[#F97316]"
+                    />
+                  </div>
+                </div>
+                <div class="font-[300] leading-[180%]">Location: USA</div>
+                <div class="flex justify-between">
+                  <div class="font-[300] leading-[180%]">Client: Luke</div>
+                  <div
+                    class="flex items-center justify-center font-[300] leading-[180%]"
+                  >
+                    2023
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-2">
+        <div class="w-[60%]">
+          <div class="relative group w-full h-[480px] cursor-pointer">
+            <NuxtImg
+              src="/Extorior/9.png"
+              class="w-full h-[480px] break-inside-avoid object-cover object-center"
+            />
+
+            <div
+              class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+            >
+              <div>
+                <div class="text-[#FFFFFF] text-justify text-[20px]">
+                  <div class="flex justify-between">
+                    <div class="font-[600] leading-[200%]">Name</div>
+                    <div class="flex justify-center items-center">
+                      <Icon
+                        name="heroicons:arrows-pointing-out-16-solid"
+                        class="size-5 text-[#F97316]"
+                      />
+                    </div>
+                  </div>
+                  <div class="font-[300] leading-[180%]">Location: USA</div>
+                  <div class="flex justify-between">
+                    <div class="font-[300] leading-[180%]">Client: Luke</div>
+                    <div
+                      class="flex items-center justify-center font-[300] leading-[180%]"
+                    >
+                      2023
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-[40%]">
+          <div class="relative group w-full h-[480px] cursor-pointer">
+            <NuxtImg
+              src="/Extorior/10.png"
+              class="w-full h-[480px] break-inside-avoid object-cover object-center"
+            />
+            <div
+              class="pt-8 px-10 pb-5 absolute bottom-0 left-0 w-full h-fit bg-black bg-opacity-[50%] opacity-0 group-hover:opacity-100 transition-opacity"
+            >
+              <div>
+                <div class="text-[#FFFFFF] text-justify text-[20px]">
+                  <div class="flex justify-between">
+                    <div class="font-[600] leading-[200%]">Name</div>
+                    <div class="flex justify-center items-center">
+                      <Icon
+                        name="heroicons:arrows-pointing-out-16-solid"
+                        class="size-5 text-[#F97316]"
+                      />
+                    </div>
+                  </div>
+                  <div class="font-[300] leading-[180%]">Location: USA</div>
+                  <div class="flex justify-between">
+                    <div class="font-[300] leading-[180%]">Client: Luke</div>
+                    <div
+                      class="flex items-center justify-center font-[300] leading-[180%]"
+                    >
+                      2023
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div class="flex w-full justify-center items-center py-10">
+          <UButton
+            color="gray"
+            variant="solid"
+            type="submit"
+            form="contactForm"
+            class="bg-gradient-to-r from-[#8D7662] to-[#27211B] lg:px-8 lg:py-5 px-6 py-4 hover:bg-[#90755e] rounded-[8px] md:w-auto"
+          >
+            <div
+              class="w-full flex justify-center items-center uppercase text-[#F5F5F5] leading-[16px] font-[600] md:text-[28px] text-[16px]"
+            >
+              see more our projects
+            </div>
+          </UButton>
+        </div>
+      </div>
     </div>
   </div>
 </template>
