88c96cffc61b553e5d1d3a9e6cab28b97b3638e2
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-06-04 15:10:24 +0700

    fix : Fixing ui Pricing

--- components/pricing/pricing-range.vue
+++ components/pricing/pricing-range.vue
@@ -139,7 +139,7 @@
                             </UCarousel>
                         </div>
                     </div>
-                    <div class="md:mt-[70px] md:pb-0 pb-5 px-3 md:px-0">
+                    <div class="md:mt-0 md:pb-0 pb-5 px-3 md:px-0">
                         <div v-if="item.subtitle"
                             class="font-[600] md:text-[32px] text-[18px] leading-[150%] text-[#8D7662] md:mb-3 mb-2">
                             {{ item.subtitle }}
