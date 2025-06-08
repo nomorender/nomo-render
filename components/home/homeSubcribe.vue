59ecaa8dc24e38ef0c305e961b801b96c9f3e88f
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-06-08 12:20:17 +0700

    fix: autofocus

--- components/home/homeSubcribe.vue
+++ components/home/homeSubcribe.vue
@@ -123,8 +123,8 @@ onMounted(() => {
               <Form class="w-full" :validate="validate">
                 <FormField name="email">
                   <UFormGroup class="flex items-center justify-center md:block ">
-                    <UInput autofocus padded required color="white" variant="none" placeholder="Your email"
-                      v-model="state.email" size="xl" class="bg-none px-2 rounded-[8px] w-[380px] md:w-full"
+                    <UInput padded required color="white" variant="none" placeholder="Your email" v-model="state.email"
+                      size="xl" class="bg-none px-2 rounded-[8px] w-[380px] md:w-full"
                       :ui="{ placeholder: 'placeholder-black font-[300] text-[20px] md:text-[25px] leading-[200%]' }" />
                   </UFormGroup>
                 </FormField>
