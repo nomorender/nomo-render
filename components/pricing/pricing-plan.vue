1dd185620796e780d51f33fdfe1938c72729a65a
nomorender <team@nomorender.com>
2025-07-03 20:12:01 +0700

    2011030725

--- components/pricing/pricing-plan.vue
+++ components/pricing/pricing-plan.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const data = {
     criteria: [
-        "Quality Control",
+        "Control",
         "Cost Efficiency",
         "Approx. Time to Finish",
         "Flexibility",
