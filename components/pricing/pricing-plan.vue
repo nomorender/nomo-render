e09b7c196deb2c4df28c3ffe899cc718b8a3880e
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-07-03 20:22:54 +0700

    fix

--- components/pricing/pricing-plan.vue
+++ components/pricing/pricing-plan.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const data = {
     criteria: [
-        "Control AAAAA",
+        "Control",
         "Cost Efficiency",
         "Approx. Time to Finish",
         "Flexibility",
